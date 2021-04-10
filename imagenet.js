379fe0cdf72864bc807064cbe0ed0bc5e4b27cdc
Om Duggineni <omduggineni@gmail.com>
2021-04-10 14:49:53 -0400

    worked on imagenet

--- imagenet.js
+++ imagenet.js
@@ -1,19 +1,18 @@
 const tf = require('@tensorflow/tfjs')
-const tf = require('@tensorflow/tfjs_node')
+const tf_node = require('@tensorflow/tfjs-node')
 const mobilenet = require('@tensorflow-models/mobilenet');
 const fs = require('fs')
 const util = require('util')
-const jimp = require('jimp');
 
 const model = ((async() => {
   return await mobilenet.load()
 })())
 
 async function imagenet_classify(image_path){
-  image = await jimp.read(image_path)
-  console.log(image.bitmap.data)
-  //predictions = await model.classify(image)
-  //return predictions
+  imageBuffer = await util.promisify(fs.readFile)(image_path)
+  const image = tf_node.node.decode_image(imageBuffer)
+  predictions = await model.classify(image)
+  return predictions
 }
 
 exports.classify = imagenet_classify
