5cae2875bdcdad2ee4bfce089f9d4b167dc360c6
Om Duggineni <omduggineni@gmail.com>
2021-04-10 11:59:31 -0400

    changed license to GPLv3 and some edits

--- main.js
+++ main.js
@@ -14,9 +14,6 @@ function createWindow () {
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
-
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
 }
 
 // This method will be called when Electron has finished
@@ -24,7 +21,7 @@ function createWindow () {
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   createWindow()
-  
+
   app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
