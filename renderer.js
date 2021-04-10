fe859b551a653bf354fc087166ca57c5ffb63614
Om Duggineni <omduggineni@gmail.com>
2021-04-10 12:09:22 -0400

    added api to Renderer process

--- renderer.js
+++ renderer.js
@@ -1,10 +1,5 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// No Node.js APIs are available in this process because
-// `nodeIntegration` is turned off. Use `preload.js` to
-// selectively enable features needed in the rendering
-// process.
+// This file is the Javascipt file for the web browser window. We can use it to run any frontend ops and call any function in the backend (code for that here)
 
 ipc.handle('perform-action', (event, ...args) => {
-  // ... do actions on behalf of the Renderer
+  // run functions in the backend
 })
