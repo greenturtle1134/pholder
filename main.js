5df8ba01ccf106d03e2067a60aaacb66cd79c04a
Om Duggineni <omduggineni@gmail.com>
2021-04-10 22:12:27 -0400

    quit when all windows are closed

--- main.js
+++ main.js
@@ -28,9 +28,9 @@ app.whenReady().then(() => {
   });
 });
 
-// Quit when all windows are closed, except on macOS.
+// Quit when all windows are closed
 app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
+  app.quit()
 });
 
 ipcMain.on("add-image", (event, path)=>{
