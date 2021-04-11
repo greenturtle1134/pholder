29fbe1ff9fe2ae22505799d8416b6c07a5d4b4d5
Om Duggineni <omduggineni@gmail.com>
2021-04-10 22:09:30 -0400

    readded icon

--- main.js
+++ main.js
@@ -10,7 +10,8 @@ function createWindow () {
     height: 600,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
-    }
+    },
+    icon: path.join(__dirname, "icon.png")
   })
 
   mainWindow.loadFile('index.html');
